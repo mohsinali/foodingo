7bec29de48b3a97e1b81153b5927d2e69aeb13de
Mohsin Ali <mohammadmohsin@gmail.com>
2015-07-16 10:44:18 +0500

    Analytics, User frequency.

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,19 +2,21 @@ class AnalyticsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @history = MealHistory.history
+    @history = MealHistory.history current_user.parse_merchant_id
 
     frequency = Hash.new
+    
     @history.each do |h|
       hour = h["createdAt"].to_datetime.hour
+      
       if frequency.has_key? hour
         frequency[hour] = frequency[hour].to_i + 1
       else
         frequency[hour] = 1
       end      
     end
-    
-    gon.keys = frequency.keys.sort.map { |e| "%02d" % e + ":00" }
+    gon.y_axis = frequency.values.sort
+    gon.x_axis = frequency.keys.sort.map { |e| "%02d" % e + ":00" }
 
   end
 
--- app/models/meal_history.rb
+++ app/models/meal_history.rb
@@ -1,9 +1,12 @@
 class MealHistory < ActiveRecord::Base
   MEAL_HISTORY_CLASS = "mealhistory"
+  MERCHANT_CLASS = "merchants"
 
 
-  def self.history
+  def self.history parse_merchant_id
+    pointer = Parse::Pointer.new({"className" => MERCHANT_CLASS, "objectId" => parse_merchant_id})
     Parse::Query.new(MEAL_HISTORY_CLASS).tap do |q|
+      q.eq("merchant_id", pointer)
       q.order_by = "createdAt"
       q.order    = :ascending
     end.get
