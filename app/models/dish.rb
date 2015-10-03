b63b31161bde385265b33d06e94cdf361ff6ea13
Mohsin Ali <mohammadmohsin@gmail.com>
2015-10-03 14:33:37 +0500

    Descitpion field with dish

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -6,6 +6,7 @@ class Dish < ActiveRecord::Base
 		dish["dish"] = dish_attributes["name"]
 		dish["price"] = dish_attributes["price"].to_i
 		dish["cafedb_id"] = dish_attributes["cafedb_id"]
+    dish["description"] = dish_attributes["description"]
 		dish.save
 	end
 
