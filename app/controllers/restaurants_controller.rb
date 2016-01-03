3334e0582a7e751b8f5fe28ec1ac44b32adcc942
Mohsin Ali <mohammadmohsin@gmail.com>
2016-01-03 19:26:33 +0500

    Edit restarutant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,9 +14,25 @@ class RestaurantsController < ApplicationController
     @dishes = Restaurant.dishes @restaurant["objectId"]
   end
 
+  def edit
+    @restaurant = Restaurant.get_restaurant params[:id]
+  end
+
   def create
     create_restaurant params[:restaurant], current_user
     
     redirect_to restaurants_path, notice: "Your restaurant has been added successfully."
+  end
+
+  def update
+    @restaurant = Restaurant.get_restaurant params[:id]
+    restaurant = params["restaurant"]
+    @restaurant["cafename"] = restaurant["cafename"]
+    @restaurant["cafelocation"] = restaurant["cafelocation"]
+
+    result = @restaurant.save
+    puts result
+
+    redirect_to restaurants_path(id: restaurant["id"])
   end
 end
