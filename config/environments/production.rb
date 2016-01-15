148f60ae89f0c4deaf82fbb8c362fa6028c683fe
Yahya <yahyawasim@live.com>
2016-01-15 12:52:56 +0500

    Listed parse users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Parse.init :application_id => ENV["PARSE_APP_ID"], # required
+             :api_key        => ENV["PARSE_APP_KEY"], # required
+             :quiet          => false
 end
